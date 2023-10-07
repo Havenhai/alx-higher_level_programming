a6c7feff0c527f19d42bc666733f30b679dc103d
Havenhai <havenhailemariam12@gmail.com>
2023-10-07 11:48:25 -0700

    try

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,11 +2,8 @@
 # 0-print_list_integer.py
 
 def print_havu_list_integer(havu_list=[]):
-    """
-    Print all integers in the provided list.
-
+    """Print all integers in the provided list.
     Args:
-        int_list (list[int]): The list containing integers to be printed.
-    """
+        int_list (list[int]): The list containing integers to be printed."""
     for i_haven in range(len(havu_list)):
         print("{:d}".format(havu_list[i_haven]))
