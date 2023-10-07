129d881bc99eec9c28745b6154767ce929845c43
Havenhai <havenhailemariam12@gmail.com>
2023-10-07 13:24:26 -0700

    bound

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 1-element_at.py
+
+
+def element_at(my_havulist, idx_havu):
+    """Retrive an element from a list."""
+    # Check if the index is out of bounds (negative or greater than the last valid index).
+    if idx_havu < 0 or idx_havu > (len(my_havulist) - 1):
+        return None
+    # Retrieve and return the element at the specified index.
+    return (my_havulist[idx_havu])
