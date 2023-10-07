a83587cbe267e6a10a704707c6e9a45ce67817fd
Havenhai <havenhailemariam12@gmail.com>
2023-10-07 11:46:04 -0700

    try

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 # 0-print_list_integer.py
 
-def print_integers(int_list=None):#
+def print_havu_list_integer(havu_list=[]):
     """
     Print all integers in the provided list.
 
     Args:
         int_list (list[int]): The list containing integers to be printed.
     """
-    if int_list is None:
-        int_list = []
-
-    for num in int_list:
-        if isinstance(num, int):
-            print("{:d}".format(num))
+    for i_haven in range(len(havu_list)):
+        print("{:d}".format(havu_list[i_haven]))
