b08b0e88ebf2044ae244c706f5161d85a5ce1ef7
Havenhai <havenhailemariam12@gmail.com>
2023-10-08 12:25:07 -0700

    try

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,9 +4,9 @@
 
 def replace_in_list(my_havu_list, idx_havu, element_havu):
     """Replace element of list at a specific position."""
-    """ Check if the provided index is within the list length)."""
+    #""" Check if the provided index is within the list length)."""
     if idx_havu >= 0 and idx_havu < len(my_havu_list):
-    """the index is valid, replace the element index with provided element."""
-     my_havu_list[idx_havu] = element_havu
-    """Return the modified list"""
+    #"""the index is valid, replace the element index with provided element."""
+        my_havu_list[idx_havu] = element_havu
+   #"""Return the modified list"""
     return (my_havu_list)
