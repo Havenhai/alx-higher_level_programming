00cbf2e342914b7cf5cee9f0d66c5d6121c62cfb
Havenhai <havenhailemariam12@gmail.com>
2023-10-09 12:53:29 -0700

    data structure

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,9 +2,9 @@
 # 2-replace_in_list.py
 
 
-def replace_in_list(my_haven_list, idx_haven, element_haven):
+def replace_in_list(my_list, idx, element):
     """Replace an element of a list at a specific position."""
-    if idx_haven >= 0 and idx_haven < len(my_haven_list):
-        my_haven_list[idx_haven] = element_haven
-    return (my_haven_list)
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list
 
